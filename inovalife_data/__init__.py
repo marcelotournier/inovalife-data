eba8fd49516ae61a036feeb4ac798891256812a1
Marcelo Tournier <15373278+marcelotournier@users.noreply.github.com>
2021-06-02 10:05:33 -0700

    Update __init__.py

--- inovalife_data/__init__.py
+++ inovalife_data/__init__.py
@@ -23,32 +23,10 @@ class DataLakeClient(SparkSession):
       "SIM_DOFET": self.spark.read.option("header", "true").csv(self.base_uri + "Base=SIM_DOFET" + "/")
     }
     
-    self.bases["SIM_DORES"].registerTempTable()
+    self.bases["SIM_DORES"].registerTempTable("SIM_DORES")
+    self.bases["SIM_DOFET"].registerTempTable("SIM_DOFET")
     
     self.tables = self.bases.keys()
     
+    self.sql = self.spark.sql
     
-
-df = spark.read.csv("s3a://datasus/Base=SINASC/Tabela=DN/UF=AC/Ano=1996/DNAC1996.csv.gz")
-
-df.show()
-
-df.write.mode("overWrite").parquet("s3a://datasus/testParquet.parquet")
-
-
-from inovalife_data import DataLakeClient
-
-access_key = "ABC"
-secret_key = "SHHHH"
-
-data = DataLakeClient(access_key, secret_key)
-
-Check all SQL tables we have:
-data.tables
-# ["table1", "table2", "table3"]
-
-# Query a table in a Spark DF format:
-df = data.sql("select * from table1")
-
-# Transform it to Pandas:
-df = df.toPandas()
